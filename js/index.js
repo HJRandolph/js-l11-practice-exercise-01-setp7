c9bc60c0266b0f1887a6e1cbf6c7cbcb733c9664
HJRandolph <hjrandolph@gmail.com>
2021-05-13 12:49:56 -0400

    Communicating with an API. Random user generator.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,40 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function (){
-    const usersRequest = await fetch("https://randomuser.me/api/?results=5");
-    console.log(usersRequest);
+const getData = async function (numUsers){
+    const usersRequest = await fetch(`https://randomuser.me/api/?results=${numUsers}`);
+    // console.log(usersRequest);
     const data = await usersRequest.json();
-    console.log(data);
+    // console.log(data);
+    const userResults = data.results;
+    // console.log(userResults);
+    displayUsers(userResults);
+    console.log(numUsers);
 };
-getData();
+
+
+const displayUsers = function(userResults){
+    randomFolks.innerHTML= "";
+    for (const user of userResults){
+        const country = user.location.country;
+        const name = user.name.first + " " + user.name.last;
+        const imageUrl = user.picture.medium;
+        console.log(imageUrl);
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+            <h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${imageUrl} alt="User Avatar" />
+        `;
+        randomFolks.append(userDiv);
+    }
+    
+};
+
+getData(1);
+
+selectUserNumber.addEventListener("change", function(e){
+    const numUsers = e.target.value;
+    getData(numUsers);
+});
+
